972a61360ee91fdf2cbce5c6047af16d8d236d97
apellet <58150232+apellet@users.noreply.github.com>
2021-05-10 21:11:25 -0300

    Added custom constructors to the Product class

--- ProductManagement/src/labs/pm/data/Product.java
+++ ProductManagement/src/labs/pm/data/Product.java
@@ -44,7 +44,23 @@ public class Product {
     private int id;
     private String name;
     private BigDecimal price;
+    private Rating rating;
 
+    public Product() {
+        this(0, "no name", BigDecimal.ZERO);
+    }
+    
+    public Product(int id, String name, BigDecimal price) {
+        this(id, name, price, Rating.NOT_RATED);
+    }
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+    
     public int getId() {
         return id;
     }
@@ -67,6 +83,14 @@ public class Product {
 
     public void setPrice(BigDecimal price) {
         this.price = price;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
     }
 
     /**
